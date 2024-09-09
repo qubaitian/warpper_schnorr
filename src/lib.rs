c33bf05e81ca3c377568734c4173359f49059cac
qubaitian <2501055750@qq.com>
2024-09-09 16:13:50 +0800

    update pyi

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,22 @@
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
+/// Formats
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+fn sum_as(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+
+/// please
+#[pyfunction]
+fn other_function(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn warpper_schnorr(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(sum_as, m)?)?;
+    m.add_function(wrap_pyfunction!(other_function, m)?)?;
     Ok(())
 }
